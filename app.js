7159a7846717f57c3448a1b4e18ca1a65f3c6596
M0Rek <morek.090@gmail.com>
2023-05-29 11:18:13 +0200

    Develop new deployment sequence

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-const express = require("express")
-const app = express()
-const axios = require("axios")
-const cors = require("cors")
-app.use(express.json())
-app.use(cors())
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
 
-const BACKEND_SERVER_IP = `http://35.228.155.54` //EDIT THIS LINE TO UPDATE THE IP (WITHOUT SLASH AT THE END)
+const BACKEND_SERVER_IP = `http://35.228.155.54`; //EDIT THIS LINE TO UPDATE THE IP (WITHOUT SLASH AT THE END)
 
 app.all("*", async (req, res) => {
-	res.setHeader(
-		"Access-Control-Allow-Origin",
-		req.header("origin") ||
-			req.header("x-forwarded-host") ||
-			req.header("referer") ||
-			req.header("host")
-	)
-	res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS")
-	try {
-		console.log(req.path)
-		const url = `${BACKEND_SERVER_IP}${req.path}`
-		const result = await axios({
-			method: req.method,
-			url: url,
-			data: req.body,
-			headers: {
-				Authorization: req.headers["authorization"],
-			},
-		})
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    req.header("origin") ||
+      req.header("x-forwarded-host") ||
+      req.header("referer") ||
+      req.header("host")
+  );
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  try {
+    console.log(req.path);
+    const url = `${BACKEND_SERVER_IP}${req.path}`;
+    const result = await axios({
+      method: req.method,
+      url: url,
+      data: req.body,
+      headers: {
+        Authorization: req.headers["authorization"],
+      },
+    });
 
-		res.status(result.status).json(result.data)
-	} catch (error) {
-		console.error(error)
-		res.status(500).send(error)
-	}
-})
+    res.status(result.status).json(result.data);
+  } catch (error) {
+    console.error(error);
+    res.status(501).send(error);
+  }
+});
 
-module.exports = app
+module.exports = app;
